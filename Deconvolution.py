d39fd679e903a63730d1c810374b3bc8bb25b962
MacDumi <dumitru_duca@outlook.com>
2018-04-15 02:07:26 +0200

    working deconvolution

--- Deconvolution.py
+++ Deconvolution.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import pandas as pd
 import numpy as np
 import math as mt
 import matplotlib.pyplot as plt
@@ -7,10 +6,26 @@ import matplotlib
 import peakutils
 from sys import argv
 from scipy.optimize import curve_fit
-from lmfit import Model, Parameters
-from scipy.stats import norm
 from scipy import asarray as ar,exp
-from joblib import Parallel, delayed
+
+####################################
+#Parameters
+degree = 1 #polynomial degree for the baseline
+
+#initial position (PAH, D4, D1, D3, G, D2)
+freq = [1280, 1168, 1353, 1517, 1585, 1611] 
+
+#shape of the peaks (PAH, D4, D1, D3, G, D2)
+shape = ['L', 'L', 'L', 'G', 'L', 'L'] 
+
+#bounds
+lower = [0, 0, 1230, 0, 0, 1127, 0, 0, 1343, 0, 0, 1489, 0, 0, 1571, 0, 0, 1599]
+upper = [np.inf, np.inf, 1300, np.inf, np.inf, 1208, np.inf, np.inf, 1358, np.inf, 
+	    np.inf, 1545, np.inf, np.inf, 1598, np.inf, np.inf, 1624]
+#data limits
+limit = [820, 2000]
+
+####################################
 
 font = {'family': 'serif',
 		'color':  'darkred',
@@ -22,67 +37,70 @@ path =argv[1]
 
 def Peak(x, pars):
 	I = pars[0]  # peak height
-	x0 = pars[1]  # centre
-	gamma = pars[2]  # ~widht
+	gamma = pars[1]  # ~widht
+	x0 = pars[2]  # centre
 	if (pars[3] =='G'): #if gaussian
-		return a*exp(-(x-x0)**2/(2*gamma**2))
+		return I*exp(-(x-x0)**2/(2*gamma**2))
 	elif (pars[3] =='L'): #if lorentzian
 		return I * gamma**2 / ((x - x0)**2 + gamma**2)
 	else:
 		print("unknown parameter")
 		return 0
-	
 
 def six_peaks(t, *pars):    
 	'function of six overlapping peaks'
-	p1 = Peak(t, [pars[0], pars[1], pars[2], 'L'])
-	p2 = Peak(t, [pars[3], pars[4], pars[5], 'G'])
-	p3 = Peak(t, [pars[6], pars[7], pars[8], 'L'])
-	p4 = Peak(t, [pars[9], pars[10], pars[11], 'L'])
-	p5 = Peak(t, [pars[12], pars[13], pars[14], 'L'])
-	p6 = Peak(t, [pars[15], pars[16], pars[17], 'L'])
+	p1 = Peak(t, [pars[0], pars[1], pars[2], shape[0]])
+	p2 = Peak(t, [pars[3], pars[4], pars[5], shape[1]])
+	p3 = Peak(t, [pars[6], pars[7], pars[8], shape[2]])
+	p4 = Peak(t, [pars[9], pars[10], pars[11], shape[3]])
+	p5 = Peak(t, [pars[12], pars[13], pars[14], shape[4]])
+	p6 = Peak(t, [pars[15], pars[16], pars[17], shape[5]])
 	return p1 + p2 + p3 + p4 + p5 + p6
 
 def plot_peaks(t, *pars):    
-	'plot six overlapping peaks'
-	plt.plot(t, Peak(t, [pars[0], pars[1], pars[2], 'L']), label ='Peak 1')
-	plt.plot(t, Peak(t, [pars[3], pars[4], pars[5], 'G']), label ='Peak 2')
-	plt.plot(t, Peak(t, [pars[6], pars[7], pars[8], 'L']), label ='Peak 3')
-	plt.plot(t, Peak(t, [pars[9], pars[10], pars[11], 'L']), label ='Peak 4')
-	plt.plot(t, Peak(t, [pars[12], pars[13], pars[14], 'L']), label ='Peak 5')
-	plt.plot(t, Peak(t, [pars[15], pars[16], pars[17], 'L']), label ='Peak 6')
+	'plot six overlapping peaks (PAH, D4, D1, D3, G, D2)'
+	plt.plot(t, Peak(t, [pars[0], pars[1], pars[2], shape[0]]), label ='PAH')
+	plt.plot(t, Peak(t, [pars[3], pars[4], pars[5], shape[1]]), label ='D4')
+	plt.plot(t, Peak(t, [pars[6], pars[7], pars[8], shape[3]]), label ='D1')
+	plt.plot(t, Peak(t, [pars[9], pars[10], pars[11], shape[3]]), label ='D3')
+	plt.plot(t, Peak(t, [pars[12], pars[13], pars[14], shape[4]]), label ='G')
+	plt.plot(t, Peak(t, [pars[15], pars[16], pars[17], shape[5]]), label ='D2')
 	
-print(argv[1])
-data = np.loadtxt(path) #load data
 
+data = np.loadtxt(path, skiprows = 2) #load data
+low = np.argwhere(data[:,0]>limit[0])[0,0]
+high = np.argwhere(data[:,0]>limit[1])[0,0]
+x = data[low:high, 0]
 #baseline
-degree = 1 #polynomial degree
-baseline = peakutils.baseline(data[:,1], degree) #baseline
-intensity = data[:,1] - baseline
+baseline = peakutils.baseline(data[low:high,1], degree) #baseline
+intensity = data[low:high,1] - baseline
 
 #plot the baseline
+'''
 fig = plt.figure(figsize=(12,8))
 ax = fig.add_subplot(111)
 ax.plot(data[:,0], data[:,1], label = 'Experimental data')
 ax.plot(data[:,0], baseline, 'r--', label = 'Baseline')
-#initial guess
-parguess = (1000, 100, 2500, 50, 1500, 20, 2000, 10, 450, 13, 500, 10, 500, 10)
-#fit the data
 '''
-popt, pcov = curve_fit(six_peaks, data[:,0], data[:,1], parguess)
+
+#initial guess
+parguess = (1000, 85, freq[0], 1000, 228, freq[1], 1000, 192, freq[2], 
+	    1000, 158, freq[3], 1000, 74, freq[4], 1000, 52, freq[5])
+#fit the data
+popt, pcov = curve_fit(six_peaks, x, intensity, parguess, bounds = [lower, upper])
 print (popt) #fit result
 #plot everything
-'''
+
 fig_res = plt.figure(figsize=(12,8))
 ax_r = fig_res.add_subplot(111)
-ax_r.plot(data[:,0], intensity,label='Experimental data')
-'''
-plt.plot(t, six_peaks(t, *popt), 'r-', label='Cumulative')
-plot_peaks(t, *popt);
+ax_r.plot(x, intensity,label='Experimental data')
+
+plt.plot(x, six_peaks(x, *popt), 'r--', label='Cumulative')
+plot_peaks(x, *popt);
 plt.ylabel("Intensity")
 plt.xlabel("cm-1")
 plt.legend()
-'''
+
 
 plt.show()
 
