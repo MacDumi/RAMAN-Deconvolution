f7afb39d2c336824e514908ac8daf0a6b42fc79d
MacDumi <dumitru_duca@outlook.com>
2018-05-29 17:20:42 +0200

    FWHM and ratios between peaks

--- Deconvolution.py
+++ Deconvolution.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 import peakutils
 import argparse as ap
 import glob
 import os
+from collections import OrderedDict
 from scipy.optimize import curve_fit
 from scipy import asarray as ar,exp
 
@@ -39,6 +41,19 @@ font = {'family': 'serif',
 		'weight': 'normal',
 		'size': 14,
 		}
+#Linestyles
+linestyles = OrderedDict(
+    [
+     ('densely dotted',      (0, (1, 1))),
+
+     ('densely dashed',      (0, (5, 1))),
+
+     ('dashdotted',          (0, (3, 4, 1, 4))),
+     ('densely dashdotted',  (0, (3, 1, 1, 1))),
+
+     ('dashdotdotted',         (0, (3, 5, 1, 5, 1, 5))),
+     ('densely dashdotdotted', (0, (3, 1, 1, 1, 1, 1)))])
+ln_style = list(linestyles.items())
 answ = ['n', 'N']
 thrsh = 0.08
 def Peak(x, pars):
@@ -56,44 +71,58 @@ def Peak(x, pars):
 def six_peaks(t, *pars):
 	'function of six overlapping peaks'
 	global six
-	p1 = Peak(t, [pars[0], pars[1], pars[2], shape[0]])
-	p2 = Peak(t, [pars[3], pars[4], pars[5], shape[1]])
-	p3 = Peak(t, [pars[6], pars[7], pars[8], shape[2]])
-	p4 = Peak(t, [pars[9], pars[10], pars[11], shape[3]])
-	p5 = Peak(t, [pars[12], pars[13], pars[14], shape[4]])
-	if six:
-		p6 = Peak(t, [pars[15], pars[16], pars[17], shape[5]])
-		return p1 + p2 + p3 + p4 + p5 + p6
-	else:
-		return p1 + p2 + p3 + p4 + p5
+	nm=names
+	if not six:
+		nm = names[:-1]
+	return np.sum([Peak(t, [pars[3*i], pars[3*i+1], pars[3*i+2], shape[i]]) for i in np.arange(0, len(nm))], axis = 0)
+
+def FWHM(X,Y):
+	difference = max(Y) - min(Y)
+
+	HM = difference / 2
+
+	pos_extremum = Y.idxmax()  # or in your case: arr_y.argmin()
+
+	nearest_above = (np.abs(Y[pos_extremum:-1] - HM)).idxmin()
+	nearest_below = (np.abs(Y[0:pos_extremum] - HM)).idxmin()
+	return  (np.mean(X[nearest_above ]) - np.mean(X[nearest_below]))
 
 
-def plot_peaks(t, *pars):
+def plot_peaks(t, axis, bsline, *pars):
 	'plot six overlapping peaks (PAH, D4, D1, D3, G, D2)'
 	global six
-	plt.plot(t, Peak(t, [pars[0], pars[1], pars[2], shape[0]]), label ='D2')
-	plt.plot(t, Peak(t, [pars[3], pars[4], pars[5], shape[1]]), label ='D4')
-	plt.plot(t, Peak(t, [pars[6], pars[7], pars[8], shape[2]]), label ='D1')
-	plt.plot(t, Peak(t, [pars[9], pars[10], pars[11], shape[3]]), label ='D3')
-	plt.plot(t, Peak(t, [pars[12], pars[13], pars[14], shape[4]]), label ='G')
-	if six:
-		plt.plot(t, Peak(t, [pars[15], pars[16], pars[17], shape[5]]), label ='PAH')
-
-def print_result(t, item, save, verbose, *pars):
-	text = "****************FIT RESULTS****************\n"
 	nm=names
 	if not six:
 		nm = names[:-1]
 	for i in np.arange(0, len(nm)):
-		text = text +"Peak %s:\n	Centre: %.4f cm-1\n	Amplitude: %.4f\n	gamma: %.4f\n" %(names[i], pars[3*i+2], pars[3*i], pars[3*i+1])
-		area = np.trapz(Peak(t, [pars[3*i], pars[3*i+1], pars[3*i+2], shape[i]]), x = t)
-		text = text +"	Area = %f\n" %area
+		axis.plot(t, Peak(t, [pars[3*i], pars[3*i+1], pars[3*i+2], shape[i]]) +bsline, linewidth = 2,linestyle = ln_style[i][1], label =nm[i])
+
+def print_result(t, out, item, save, verbose, pars):
+	text = "****************BASELINE*******************\nSlope: %.4f\nIntercept: %.4f\n"%(pars[0], pars[1])
+	nm=names
+	area =[]
+	intensity = []
+	text = text +"\n****************FIT RESULTS****************\n"
+	if not six:
+		nm = names[:-1]
+	for i in np.arange(0, len(nm)):
+		out[nm[i]] = Peak(t, [pars[3*i+2], pars[3*i+3], pars[3*i+4], shape[i]])
+		fwhm = FWHM(t, out[nm[i]])
+		text = text +"Peak %s:\n	Centre: %.4f cm-1\n	Amplitude: %.4f\n	gamma: %.4f\n	FWHM: %.4f\n" %(names[i], pars[3*i+4], pars[3*i+2], pars[3*i+3], fwhm)
+		area = np.append(area ,np.trapz(Peak(t, [pars[3*i+2], pars[3*i+3], pars[3*i+4], shape[i]]), x = t))
+		intensity = np.append(intensity, pars[3*i+2])
+		text = text +"	Area = %f\n" %area[i]
+	out['Cumulative'] = np.sum(out.values[:,3:], axis=1)
+	text = text +"\n**************Ratio - Amplitude************\n	D1/G= %.4f\n	D1/(G+D1+D2)= %.4f\n	D4/G= %.4f\n" %(intensity[2]/intensity[4], intensity[2]/(intensity[4]+intensity[0]+intensity[2]), intensity[1]/intensity[4])
+	text = text +"\n**************Ratio - Areas****************\n	D1/G= %.4f\n	D1/(G+D1+D2)= %.4f\n	D4/G= %.4f\n" %(area[2]/area[4], area[2]/(area[4]+area[0]+area[2]), area[1]/area[4])
+	
 	if verbose:
 		print(text)
 	if save:
-		output = open(item[:-4]+"_fit_result.txt", "w")
+		output = open(item[:-4]+"/fit_result.txt", "w")
 		output.writelines(text)
 		output.close()
+		out.to_csv(item[:-4]+"/data.csv", index=None)
 
 def detect_spikes(y):
 	#detect spikes
@@ -108,23 +137,28 @@ def detect_spikes(y):
 	return [int(spk) for spk in spikes]
 
 def plot_baseline(x, y,baseline, spikes):
-        plt.close()
-        fig = plt.figure(figsize=(12,8))
-        ax = fig.add_subplot(111)
-        ax.plot(x, y, label = 'Experimental data')
-        if len(spikes):
-            ax.plot(x[spikes], y[spikes], 'ro', label='Spikes')
-        ax.plot(x, baseline, 'r--', label = 'Baseline')
-        plt.ylabel("Intensity")
-        plt.xlabel("cm-1")
-        plt.legend()
-        plt.grid()
-        plt.show(block=False)
+		plt.close()
+		fig = plt.figure(figsize=(12,8))
+		ax = fig.add_subplot(111)
+		ax.plot(x, y, label = 'Experimental data')
+		if len(spikes):
+			ax.plot(x[spikes], y[spikes], 'ro', label='Spikes')
+		ax.plot(x, baseline, 'r--', label = 'Baseline')
+		plt.ylabel("Intensity")
+		plt.xlabel("cm-1")
+		plt.legend()
+		plt.grid()
+		plt.show(block=False)
+		return fig
 
 def deconvolute(item, save, verbose, bs_line):
 	global six, thrsh, degree
 	menu_flag = False
 	bs_flag = True
+	if save:
+		if not os.path.exists(item[:-4]):
+			os.makedirs(item[:-4])
+	
 	data = np.loadtxt(item, skiprows = 2) #load data
 	if data[0,0]>limit[0]:
 		limit[0]=data[0,0]
@@ -134,8 +168,14 @@ def deconvolute(item, save, verbose, bs_line):
 	high = np.argwhere(data[:,0]>limit[1])[0,0]
 	x = data[low:high, 0]
 	y= data[low:high, 1]
+	
+	
 	#baseline
 	baseline = peakutils.baseline(y, degree) #baseline
+	if degree ==1:
+		slope, intercept = np.polyfit(x, baseline, 1)
+	else:
+		slope, intercept = -1, -1
 	intensity = y - baseline
 	if spike_detect:
 		spikes = detect_spikes(y)
@@ -143,7 +183,7 @@ def deconvolute(item, save, verbose, bs_line):
 		spikes = []
 	#plot the baseline and spikes
 	if bs_line:
-		plot_baseline(x, y, baseline, spikes)
+		baseLN = plot_baseline(x, y, baseline, spikes)
 	while bs_flag and verbose:
 		other_deg = input("Baseline polynomial degree is set to: %d\nDo you want to change it? [N/y]" %degree)
 		if other_deg in ['Y', 'y']:
@@ -154,12 +194,12 @@ def deconvolute(item, save, verbose, bs_line):
 			degree = deg
 			baseline = peakutils.baseline(y, degree) #baseline
 			intensity = y - baseline
-			plot_baseline(x, y, baseline, spikes)
+			baseLN = plot_baseline(x, y, baseline, spikes)
 		else:
 			bs_flag =False
 
 	if spike_detect:
-            #Remove the spikes or not?
+			#Remove the spikes or not?
 		while not menu_flag:
 			if verbose:
 				if spikes:
@@ -180,7 +220,7 @@ def deconvolute(item, save, verbose, bs_line):
 						print("Only numbers are allowed")
 					print("New value is %.1f%%" %(thrsh*100))
 					spikes = detect_spikes(y)
-					plot_baseline(x, y, baseline, spikes)
+					baseLN = plot_baseline(x, y, baseline, spikes)
 			else:
 				#remove spikes
 				print("Removing the spikes")
@@ -190,7 +230,7 @@ def deconvolute(item, save, verbose, bs_line):
 					x = np.delete(x, idx)
 
 	#get the number of peaks to fit with
-	print("\n*********Deconvolution***********")
+	print("\n***********Deconvolution*************")
 	if verbose:
 		pah = input("Take into account the PAH band? [Y/n] ")
 	else:
@@ -203,28 +243,49 @@ def deconvolute(item, save, verbose, bs_line):
 		print("Fitting with the PAH band")
 
 	#weight for fitting....note:lower sigma->higher weight
-	sigma =np.ones(len(x))*1.2
+	sigma =np.ones(len(x))*2
 	sigma[np.abs(x-1450)<50]=0.8
+	sigma[np.abs(x-1000)<50]=0.8
+	sigma[np.abs(x-1900)<50]=0.7
 	#initial guess
 	parguess = (5000, 25, freq[0], 10000, 50, freq[1], 5000, 60, freq[2],
 			2000, 70, freq[3], 10000, 30, freq[4], 10000, 25, freq[5])
 	#fit the data
 	popt, pcov = curve_fit(six_peaks, x, intensity, parguess,sigma=sigma, bounds = [lower, upper])
+	#output
+	out = pd.DataFrame()
+	out['Raman shift'] = x
+	out['Raw data'] = intensity+baseline
+	out['Baseline'] = baseline
 	#fit result
-	print_result(x, item, save, verbose, *popt)
+	print_result(x, out, item, save, verbose, np.append([slope, intercept],popt))
 	#plot everything
 	fig_res = plt.figure(figsize=(12,8))
 	ax_r = fig_res.add_subplot(111)
-	ax_r.scatter(x, intensity,s=5, label='Experimental data')
+	ax_r.plot(x, six_peaks(x, *popt), 'r-',linewidth = 1, label='Cumulative')
+	plot_peaks(x, ax_r, np.zeros(len(baseline)), *popt);
+	ax_r.plot(x, intensity,'o',markersize=3, color = '#1E68FF',label='Experimental data')
+	ax_r.set_ylabel("Intensity")
+	ax_r.set_xlabel("Raman shift, cm-1")
+	ax_r.legend()
+	ax_r.grid()
+	plt.tight_layout()
 
-	plt.plot(x, six_peaks(x, *popt), 'r--', label='Cumulative')
-	plot_peaks(x, *popt);
-	plt.ylabel("Intensity")
-	plt.xlabel("cm-1")
-	plt.legend()
-	plt.grid()
+	#plot results with the baseline
+	fig_res_bsl = plt.figure(figsize=(12,8))
+	ax_r_b = fig_res_bsl.add_subplot(111)
+	ax_r_b.plot(x, six_peaks(x, *popt)+baseline, 'r-', linewidth = 1,label='Cumulative')
+	plot_peaks(x,ax_r_b, baseline, *popt);
+	ax_r_b.plot(x, y,'o',markersize=3, color = '#1E68FF', label='Experimental data')
+	ax_r_b.set_ylabel("Intensity")
+	ax_r_b.set_xlabel("Raman shift, cm-1")
+	ax_r_b.legend()
+	ax_r_b.grid()
+	plt.tight_layout()
 	if(save):
-		plt.savefig(item[:-3]+'png')
+		fig_res.savefig(item[:-4]+'/fit.png')
+		fig_res_bsl.savefig(item[:-4]+'/fit+baseline.png')
+		baseLN.savefig(item[:-4]+'/baseline.png')
 
 
 if __name__ == '__main__':
@@ -237,7 +298,7 @@ if __name__ == '__main__':
 	parser.add_argument('-f','--filter', action='store_true', help='Detects and removes spikes')
 	parser.add_argument('name', help='File name')
 	args = parser.parse_args()
-	matplotlib.rcParams.update({'font.size': 14})
+	matplotlib.rcParams.update({'font.size': 18})
 	spike_detect= args.filter
 	if (args.path):
 		if spike_detect:
@@ -248,8 +309,9 @@ if __name__ == '__main__':
 			print(item+" proccessed")
 			deconvolute(item, args.save, False, False)
 	else:
-		deconvolute(args.name, args.save, True, True)
-		plt.show()
+			deconvolute(args.name, args.save, True, True)
+			plt.tight_layout()
+			plt.show()
 	os._exit(0)
 
 
